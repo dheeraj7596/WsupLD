47f97744185b985f776bacba34ae1ee9a2c26e31
dheeraj7596 <dheeraj7596.dm@gmail.com>
2021-10-18 10:12:15 -0700

    made num_its to 3 for upperbound exps.

--- roberta_main.py
+++ roberta_main.py
@@ -26,7 +26,7 @@ if __name__ == "__main__":
     percent_thresh = float(sys.argv[6])
     bins = [0, 0.25, 0.5, 0.75, 1]
     bins_five = [0, 1, 2, 3, 4, 5]
-    num_its = 5
+    num_its = 3
     # use_gpu = 0
 
     seed_val = 42
