16b16e80ae23827d3e0e992a6e0e93bf1f2e5ac9
dheeraj7596 <dheeraj7596.dm@gmail.com>
2022-02-08 08:37:43 -0800

    nyt-fine added to bert exps

--- run_bert_exp.sh
+++ run_bert_exp.sh
@@ -1,2 +1,5 @@
-python3 train.py 1 0 nyt-coarse 4 0 0.5 > output/nyt_coarse/bert_ttest_random.txt
-python3 train.py 1 0 nyt-coarse 5 0 0.5 > output/nyt_coarse/bert_ttest_stability.txt
+python3 train.py 1 0 nyt-fine 0 0 0.5 > output/nyt_fine/bert_ttest_nofilter.txt
+python3 train.py 1 0 nyt-fine 1 0 0.5 > output/nyt_fine/bert_ttest_top50.txt
+python3 train.py 1 0 nyt-fine 2 0 0.5 > output/nyt_fine/bert_ttest_probability.txt
+python3 train.py 1 0 nyt-fine 4 0 0.5 > output/nyt_fine/bert_ttest_random.txt
+python3 train.py 1 0 nyt-fine 5 0 0.5 > output/nyt_fine/bert_ttest_stability.txt
