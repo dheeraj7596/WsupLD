a1b13585496e7f3052811d902b43807e44de9abe
dheeraj7596 <dheeraj7596.dm@gmail.com>
2022-02-09 11:38:05 -0800

    shell scripts modified.

--- run_bert_exp.sh
+++ run_bert_exp.sh
@@ -1,5 +1,5 @@
-python3 train.py 1 0 nyt-fine 0 0 0.5 > output/nyt_fine/bert_ttest_nofilter.txt
-python3 train.py 1 0 nyt-fine 1 0 0.5 > output/nyt_fine/bert_ttest_top50.txt
-python3 train.py 1 0 nyt-fine 2 0 0.5 > output/nyt_fine/bert_ttest_probability.txt
-python3 train.py 1 0 nyt-fine 4 0 0.5 > output/nyt_fine/bert_ttest_random.txt
-python3 train.py 1 0 nyt-fine 5 0 0.5 > output/nyt_fine/bert_ttest_stability.txt
+python3 train.py 1 0 20news-coarse-nomisc 0 0 0.5 > output/20news_coarse_nomisc/bert_ttest_nofilter.txt
+python3 train.py 1 0 20news-coarse-nomisc 1 0 0.5 > output/20news_coarse_nomisc/bert_ttest_top50.txt
+python3 train.py 1 0 20news-coarse-nomisc 2 0 0.5 > output/20news_coarse_nomisc/bert_ttest_probability.txt
+python3 train.py 1 0 20news-coarse-nomisc 4 0 0.5 > output/20news_coarse_nomisc/bert_ttest_random.txt
+python3 train.py 1 0 20news-coarse-nomisc 5 0 0.5 > output/20news_coarse_nomisc/bert_ttest_stability.txt
