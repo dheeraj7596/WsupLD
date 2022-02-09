b1b10c219878a4da492c06b0b3b3f32952fee576
dheeraj7596 <dheeraj7596.dm@gmail.com>
2022-02-09 11:39:38 -0800

    modified gpu id

--- run_gpt2_exp.sh
+++ run_gpt2_exp.sh
@@ -1,5 +1,5 @@
-python3 gpt2_main.py 1 3 nyt-fine 0 0 0.5 > output/nyt_fine/gpt2_ttest_nofilter.txt
-python3 gpt2_main.py 1 3 nyt-fine 1 0 0.5 > output/nyt_fine/gpt2_ttest_top50.txt
-python3 gpt2_main.py 1 3 nyt-fine 2 0 0.5 > output/nyt_fine/gpt2_ttest_probability.txt
-python3 gpt2_main.py 1 3 nyt-fine 4 0 0.5 > output/nyt_fine/gpt2_ttest_random.txt
-python3 gpt2_main.py 1 3 nyt-fine 5 0 0.5 > output/nyt_fine/gpt2_ttest_stability.txt
+python3 gpt2_main.py 1 2 nyt-fine 0 0 0.5 > output/nyt_fine/gpt2_ttest_nofilter.txt
+python3 gpt2_main.py 1 2 nyt-fine 1 0 0.5 > output/nyt_fine/gpt2_ttest_top50.txt
+python3 gpt2_main.py 1 2 nyt-fine 2 0 0.5 > output/nyt_fine/gpt2_ttest_probability.txt
+python3 gpt2_main.py 1 2 nyt-fine 4 0 0.5 > output/nyt_fine/gpt2_ttest_random.txt
+python3 gpt2_main.py 1 2 nyt-fine 5 0 0.5 > output/nyt_fine/gpt2_ttest_stability.txt
