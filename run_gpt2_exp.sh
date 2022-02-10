d9e58c6a5201bea35584e7c29a9775156dd4f302
dheeraj7596 <dheeraj7596.dm@gmail.com>
2022-02-10 12:43:54 -0800

    20news-fine added to bert, roberta and 20news-coarse added to gpt2 exps

--- run_gpt2_exp.sh
+++ run_gpt2_exp.sh
@@ -1,5 +1,5 @@
-python3 gpt2_main.py 1 2 nyt-fine 0 0 0.5 > output/nyt_fine/gpt2_ttest_nofilter.txt
-python3 gpt2_main.py 1 2 nyt-fine 1 0 0.5 > output/nyt_fine/gpt2_ttest_top50.txt
-python3 gpt2_main.py 1 2 nyt-fine 2 0 0.5 > output/nyt_fine/gpt2_ttest_probability.txt
-python3 gpt2_main.py 1 2 nyt-fine 4 0 0.5 > output/nyt_fine/gpt2_ttest_random.txt
-python3 gpt2_main.py 1 2 nyt-fine 5 0 0.5 > output/nyt_fine/gpt2_ttest_stability.txt
+python3 gpt2_main.py 1 2 20news-coarse-nomisc 0 0 0.5 > output/20news_coarse_nomisc/gpt2_ttest_nofilter.txt
+python3 gpt2_main.py 1 2 20news-coarse-nomisc 1 0 0.5 > output/20news_coarse_nomisc/gpt2_ttest_top50.txt
+python3 gpt2_main.py 1 2 20news-coarse-nomisc 2 0 0.5 > output/20news_coarse_nomisc/gpt2_ttest_probability.txt
+python3 gpt2_main.py 1 2 20news-coarse-nomisc 4 0 0.5 > output/20news_coarse_nomisc/gpt2_ttest_random.txt
+python3 gpt2_main.py 1 2 20news-coarse-nomisc 5 0 0.5 > output/20news_coarse_nomisc/gpt2_ttest_stability.txt
--- run_roberta_exp.sh
+++ run_roberta_exp.sh
@@ -1,5 +1,5 @@
-python3 roberta_main.py 1 4 20news-coarse-nomisc 0 0 0.5 > output/20news_coarse_nomisc/roberta_ttest_nofilter.txt
-python3 roberta_main.py 1 4 20news-coarse-nomisc 1 0 0.5 > output/20news_coarse_nomisc/roberta_ttest_top50.txt
-python3 roberta_main.py 1 4 20news-coarse-nomisc 2 0 0.5 > output/20news_coarse_nomisc/roberta_ttest_probability.txt
-python3 roberta_main.py 1 4 20news-coarse-nomisc 4 0 0.5 > output/20news_coarse_nomisc/roberta_ttest_random.txt
-python3 roberta_main.py 1 4 20news-coarse-nomisc 5 0 0.5 > output/20news_coarse_nomisc/roberta_ttest_stability.txt
+python3 roberta_main.py 1 4 20news-fine-nomisc 0 0 0.5 > output/20news_fine_nomisc/roberta_ttest_nofilter.txt
+python3 roberta_main.py 1 4 20news-fine-nomisc 1 0 0.5 > output/20news_fine_nomisc/roberta_ttest_top50.txt
+python3 roberta_main.py 1 4 20news-fine-nomisc 2 0 0.5 > output/20news_fine_nomisc/roberta_ttest_probability.txt
+python3 roberta_main.py 1 4 20news-fine-nomisc 4 0 0.5 > output/20news_fine_nomisc/roberta_ttest_random.txt
+python3 roberta_main.py 1 4 20news-fine-nomisc 5 0 0.5 > output/20news_fine_nomisc/roberta_ttest_stability.txt
