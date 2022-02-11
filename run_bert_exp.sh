1f4e1d5a16b4a97421ad03ed62a08f8f517ae8ec
dheeraj7596 <dheeraj7596.dm@gmail.com>
2022-02-11 10:05:34 -0800

    bert for books added.

--- run_bert_exp.sh
+++ run_bert_exp.sh
@@ -1,5 +1,5 @@
-python3 train.py 1 0 20news-fine-nomisc 0 0 0.5 > output/20news_fine_nomisc/bert_ttest_nofilter.txt
-python3 train.py 1 0 20news-fine-nomisc 1 0 0.5 > output/20news_fine_nomisc/bert_ttest_top50.txt
-python3 train.py 1 0 20news-fine-nomisc 2 0 0.5 > output/20news_fine_nomisc/bert_ttest_probability.txt
-python3 train.py 1 0 20news-fine-nomisc 4 0 0.5 > output/20news_fine_nomisc/bert_ttest_random.txt
-python3 train.py 1 0 20news-fine-nomisc 5 0 0.5 > output/20news_fine_nomisc/bert_ttest_stability.txt
+python3 train.py 1 0 books 0 0 0.5 > output/books/bert_ttest_nofilter.txt
+python3 train.py 1 0 books 1 0 0.5 > output/books/bert_ttest_top50.txt
+python3 train.py 1 0 books 2 0 0.5 > output/books/bert_ttest_probability.txt
+python3 train.py 1 0 books 4 0 0.5 > output/books/bert_ttest_random.txt
+python3 train.py 1 0 books 5 0 0.5 > output/books/bert_ttest_stability.txt
