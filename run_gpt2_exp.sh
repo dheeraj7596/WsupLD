cb69e04b12c83a7ecc7683f6f6ef9fd1d66922a9
dheeraj7596 <dheeraj7596.dm@gmail.com>
2022-02-11 10:06:29 -0800

    20news-fine for gpt2

--- run_gpt2_exp.sh
+++ run_gpt2_exp.sh
@@ -1,5 +1,5 @@
-python3 gpt2_main.py 1 2 20news-coarse-nomisc 0 0 0.5 > output/20news_coarse_nomisc/gpt2_ttest_nofilter.txt
-python3 gpt2_main.py 1 2 20news-coarse-nomisc 1 0 0.5 > output/20news_coarse_nomisc/gpt2_ttest_top50.txt
-python3 gpt2_main.py 1 2 20news-coarse-nomisc 2 0 0.5 > output/20news_coarse_nomisc/gpt2_ttest_probability.txt
-python3 gpt2_main.py 1 2 20news-coarse-nomisc 4 0 0.5 > output/20news_coarse_nomisc/gpt2_ttest_random.txt
-python3 gpt2_main.py 1 2 20news-coarse-nomisc 5 0 0.5 > output/20news_coarse_nomisc/gpt2_ttest_stability.txt
+python3 gpt2_main.py 1 2 20news-fine-nomisc 0 0 0.5 > output/20news_fine_nomisc/gpt2_ttest_nofilter.txt
+python3 gpt2_main.py 1 2 20news-fine-nomisc 1 0 0.5 > output/20news_fine_nomisc/gpt2_ttest_top50.txt
+python3 gpt2_main.py 1 2 20news-fine-nomisc 2 0 0.5 > output/20news_fine_nomisc/gpt2_ttest_probability.txt
+python3 gpt2_main.py 1 2 20news-fine-nomisc 4 0 0.5 > output/20news_fine_nomisc/gpt2_ttest_random.txt
+python3 gpt2_main.py 1 2 20news-fine-nomisc 5 0 0.5 > output/20news_fine_nomisc/gpt2_ttest_stability.txt
