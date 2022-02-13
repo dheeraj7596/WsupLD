60114bac08a6a2894fc1ac385d67069ac99c81ed
dheeraj7596 <dheeraj7596.dm@gmail.com>
2022-02-12 23:41:26 -0800

    books added to gpt2

--- run_gpt2_exp.sh
+++ run_gpt2_exp.sh
@@ -1,5 +1,5 @@
-python3 gpt2_main.py 1 2 20news-fine-nomisc 0 0 0.5 > output/20news_fine_nomisc/gpt2_ttest_nofilter.txt
-python3 gpt2_main.py 1 2 20news-fine-nomisc 1 0 0.5 > output/20news_fine_nomisc/gpt2_ttest_top50.txt
-python3 gpt2_main.py 1 2 20news-fine-nomisc 2 0 0.5 > output/20news_fine_nomisc/gpt2_ttest_probability.txt
-python3 gpt2_main.py 1 2 20news-fine-nomisc 4 0 0.5 > output/20news_fine_nomisc/gpt2_ttest_random.txt
-python3 gpt2_main.py 1 2 20news-fine-nomisc 5 0 0.5 > output/20news_fine_nomisc/gpt2_ttest_stability.txt
+python3 gpt2_main.py 1 2 books 0 0 0.5 > output/books/gpt2_ttest_nofilter.txt
+python3 gpt2_main.py 1 2 books 1 0 0.5 > output/books/gpt2_ttest_top50.txt
+python3 gpt2_main.py 1 2 books 2 0 0.5 > output/books/gpt2_ttest_probability.txt
+python3 gpt2_main.py 1 2 books 4 0 0.5 > output/books/gpt2_ttest_random.txt
+python3 gpt2_main.py 1 2 books 5 0 0.5 > output/books/gpt2_ttest_stability.txt
