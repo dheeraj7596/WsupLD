7974bc3a0f02f86f32044f7add3df95dfa37a8a0
dheeraj7596 <dheeraj7596.dm@gmail.com>
2022-05-12 23:52:27 -0700

    roberta script added.

---
+++ scripts/roberta.sh
@@ -0,0 +1,8 @@
+gpu=$1
+
+#python3 roberta_main.py 1 ${gpu} 20news-coarse-nomisc 7 0 0.5 > output/20news_coarse_nomisc/o2u_roberta.txt
+#python3 roberta_main.py 1 ${gpu} 20news-fine-nomisc 7 0 0.5 > output/20news_fine_nomisc/o2u_roberta.txt
+#python3 roberta_main.py 1 ${gpu} nyt-coarse 7 0 0.5 > output/nyt_coarse/o2u_roberta.txt
+#python3 roberta_main.py 1 ${gpu} nyt-fine 7 0 0.5 > output/nyt_fine/o2u_roberta.txt
+python3 roberta_main.py 1 ${gpu} books 7 0 0.5 > output/books/o2u_roberta.txt
+python3 -i roberta_main.py 1 ${gpu} agnews 7 0 0.5 > output/agnews/o2u_roberta.txt
