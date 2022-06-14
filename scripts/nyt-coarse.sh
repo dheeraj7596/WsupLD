9b16bf0654b45650424164ccfb3df33bd62d7a0b
dheeraj7596 <dheeraj7596.dm@gmail.com>
2022-06-14 11:02:25 -0700

    added for prob num

--- scripts/nyt-coarse.sh
+++ scripts/nyt-coarse.sh
@@ -1,8 +1,15 @@
 gpu=$1
 
-python3 train.py 1 ${gpu} nyt-coarse 6 0 0.5 1 > output/nyt_coarse/bert_batch_epoch_filter_1.txt
-python3 train.py 1 ${gpu} nyt-coarse 6 0 0.5 3 > output/nyt_coarse/bert_batch_epoch_filter_3.txt
-python3 train.py 1 ${gpu} nyt-coarse 6 0 0.5 5 > output/nyt_coarse/bert_batch_epoch_filter_5.txt
-python3 train.py 1 ${gpu} nyt-coarse 6 0 0.5 15 > output/nyt_coarse/bert_batch_epoch_filter_15.txt
-python3 train.py 1 ${gpu} nyt-coarse 6 0 0.5 30 > output/nyt_coarse/bert_batch_epoch_filter_30.txt
-python3 -i train.py 1 ${gpu} nyt-coarse 6 0 0.5 40 > output/nyt_coarse/bert_batch_epoch_filter_40.txt
+
+python3 train.py 1 ${gpu} nyt-coarse 1 0 0.3 40 > output/nyt_coarse/bert_top50_epoch_num_filter_0.3thresh.txt
+python3 train.py 1 ${gpu} nyt-coarse 1 0 0.5 40 > output/nyt_coarse/bert_top50_epoch_num_filter_0.5thresh.txt
+python3 train.py 1 ${gpu} nyt-coarse 1 0 0.7 40 > output/nyt_coarse/bert_top50_epoch_num_filter_0.7thresh.txt
+python3 train.py 1 ${gpu} nyt-coarse 1 0 0.8 40 > output/nyt_coarse/bert_top50_epoch_num_filter_0.8thresh.txt
+python3 train.py 1 ${gpu} nyt-coarse 1 0 0.9 40 > output/nyt_coarse/bert_top50_epoch_num_filter_0.9thresh.txt
+
+#python3 train.py 1 ${gpu} nyt-coarse 6 0 0.5 1 > output/nyt_coarse/bert_batch_epoch_filter_1.txt
+#python3 train.py 1 ${gpu} nyt-coarse 6 0 0.5 3 > output/nyt_coarse/bert_batch_epoch_filter_3.txt
+#python3 train.py 1 ${gpu} nyt-coarse 6 0 0.5 5 > output/nyt_coarse/bert_batch_epoch_filter_5.txt
+#python3 train.py 1 ${gpu} nyt-coarse 6 0 0.5 15 > output/nyt_coarse/bert_batch_epoch_filter_15.txt
+#python3 train.py 1 ${gpu} nyt-coarse 6 0 0.5 30 > output/nyt_coarse/bert_batch_epoch_filter_30.txt
+#python3 -i train.py 1 ${gpu} nyt-coarse 6 0 0.5 40 > output/nyt_coarse/bert_batch_epoch_filter_40.txt
--- scripts/prob-num.sh
+++ scripts/prob-num.sh
@@ -1,13 +1,25 @@
 gpu=$1
 
-python3 train.py 1 ${gpu} nyt-coarse 8 0 0.3 > output/nyt_coarse/bert_prob_num_filter_0.3thresh.txt
-python3 train.py 1 ${gpu} nyt-coarse 8 0 0.5 > output/nyt_coarse/bert_prob_num_filter_0.5thresh.txt
-python3 train.py 1 ${gpu} nyt-coarse 8 0 0.7 > output/nyt_coarse/bert_prob_num_filter_0.7thresh.txt
-python3 train.py 1 ${gpu} nyt-coarse 8 0 0.8 > output/nyt_coarse/bert_prob_num_filter_0.8thresh.txt
-python3 train.py 1 ${gpu} nyt-coarse 8 0 0.9 > output/nyt_coarse/bert_prob_num_filter_0.9thresh.txt
+#python3 train.py 1 ${gpu} nyt-coarse 1 0 0.3 40 > output/nyt_coarse/bert_top50_epoch_num_filter_0.3thresh.txt
+#python3 train.py 1 ${gpu} nyt-coarse 1 0 0.5 40 > output/nyt_coarse/bert_top50_epoch_num_filter_0.5thresh.txt
+#python3 train.py 1 ${gpu} nyt-coarse 1 0 0.7 40 > output/nyt_coarse/bert_top50_epoch_num_filter_0.7thresh.txt
+#python3 train.py 1 ${gpu} nyt-coarse 1 0 0.8 40 > output/nyt_coarse/bert_top50_epoch_num_filter_0.8thresh.txt
+#python3 train.py 1 ${gpu} nyt-coarse 1 0 0.9 40 > output/nyt_coarse/bert_top50_epoch_num_filter_0.9thresh.txt
 
-python3 train.py 1 ${gpu} 20news-fine-nomisc 8 0 0.3 > output/20news_fine_nomisc/bert_prob_num_filter_0.3thresh.txt
-python3 train.py 1 ${gpu} 20news-fine-nomisc 8 0 0.5 > output/20news_fine_nomisc/bert_prob_num_filter_0.5thresh.txt
-python3 train.py 1 ${gpu} 20news-fine-nomisc 8 0 0.7 > output/20news_fine_nomisc/bert_prob_num_filter_0.7thresh.txt
-python3 train.py 1 ${gpu} 20news-fine-nomisc 8 0 0.8 > output/20news_fine_nomisc/bert_prob_num_filter_0.8thresh.txt
-python3 train.py 1 ${gpu} 20news-fine-nomisc 8 0 0.9 > output/20news_fine_nomisc/bert_prob_num_filter_0.9thresh.txt
+#python3 train.py 1 ${gpu} 20news-fine-nomisc 1 0 0.3 40 > output/20news_fine_nomisc/bert_top50_epoch_num_filter_0.3thresh.txt
+#python3 train.py 1 ${gpu} 20news-fine-nomisc 1 0 0.5 40 > output/20news_fine_nomisc/bert_top50_epoch_num_filter_0.5thresh.txt
+#python3 train.py 1 ${gpu} 20news-fine-nomisc 1 0 0.7 40 > output/20news_fine_nomisc/bert_top50_epoch_num_filter_0.7thresh.txt
+#python3 train.py 1 ${gpu} 20news-fine-nomisc 1 0 0.8 40 > output/20news_fine_nomisc/bert_top50_epoch_num_filter_0.8thresh.txt
+#python3 train.py 1 ${gpu} 20news-fine-nomisc 1 0 0.9 40 > output/20news_fine_nomisc/bert_top50_epoch_num_filter_0.9thresh.txt
+
+#python3 train.py 1 ${gpu} nyt-coarse 8 0 0.3 > output/nyt_coarse/bert_prob_num_filter_0.3thresh.txt
+#python3 train.py 1 ${gpu} nyt-coarse 8 0 0.5 > output/nyt_coarse/bert_prob_num_filter_0.5thresh.txt
+#python3 train.py 1 ${gpu} nyt-coarse 8 0 0.7 > output/nyt_coarse/bert_prob_num_filter_0.7thresh.txt
+#python3 train.py 1 ${gpu} nyt-coarse 8 0 0.8 > output/nyt_coarse/bert_prob_num_filter_0.8thresh.txt
+#python3 train.py 1 ${gpu} nyt-coarse 8 0 0.9 > output/nyt_coarse/bert_prob_num_filter_0.9thresh.txt
+#
+#python3 train.py 1 ${gpu} 20news-fine-nomisc 8 0 0.3 > output/20news_fine_nomisc/bert_prob_num_filter_0.3thresh.txt
+#python3 train.py 1 ${gpu} 20news-fine-nomisc 8 0 0.5 > output/20news_fine_nomisc/bert_prob_num_filter_0.5thresh.txt
+#python3 train.py 1 ${gpu} 20news-fine-nomisc 8 0 0.7 > output/20news_fine_nomisc/bert_prob_num_filter_0.7thresh.txt
+#python3 train.py 1 ${gpu} 20news-fine-nomisc 8 0 0.8 > output/20news_fine_nomisc/bert_prob_num_filter_0.8thresh.txt
+#python3 train.py 1 ${gpu} 20news-fine-nomisc 8 0 0.9 > output/20news_fine_nomisc/bert_prob_num_filter_0.9thresh.txt
